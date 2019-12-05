fa0612a8163701aea64ed55c053287d266eaadb8
andrejr95 <andrelgf95@gmail.com>
2019-12-05 02:04:49 +0000

    Limpando main.c para envio

--- main.c
+++ main.c
@@ -15,32 +15,4 @@ int main() {
 
 
 
-		/* Teste sem função de leitura ok
-		
-		int i, j;
-		ponto p1, p2, p3, pcirculo;
-		Pixel_cor *cor;
-		ponto *p_1 = (ponto*) malloc(sizeof(ponto) * 3);
-		p_1->x = 200;
-		p_1->y = 200;
-		(p_1+1)->x = 400;
-		(p_1+1)->y = 300;
-		(p_1+2)->x = 350;
-		(p_1+2)->y = 350;
-		p1.x = 100;
-		p1.y = 200;
-		p2.x = 300;
-		p2.y = 350;
-		pcirculo.x = 100;
-		pcirculo.y = 100;
-		matriz = image(600, 400);
-		clear(matriz, 0, 0, 0);
-		cor = color(255, 255, 0);
-		line(p1, p2, cor, matriz);
-		polygon(p_1, 3, cor, matriz);
-		circle(pcirculo, 100, cor, matriz);
-		save("temp.ppm", matriz); */
-
-
-
 
